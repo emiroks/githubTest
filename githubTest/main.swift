f144cecdce69adc934dc8bf642ee83c7ebec994c
emiroks <emir.hotelmydora@gmail.com>
2015-06-21 00:01:51 +0300

    testing
    testing

---
+++ githubTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  githubTest
+//
+//  Created by Emirhan Erdogan on 20/06/15.
+//  Copyright (c) 2015 Emirhan Erdogan. All rights reserved.
+//
+
+import Foundation
+
+var test = 12
+var test2 = 26
